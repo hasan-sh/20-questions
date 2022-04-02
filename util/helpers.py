54025fd8ca0654c051947ffe43d7b09997601c23
Hisham Alkaed <heshamkaed2@gmail.com>
2022-04-02 17:19:57 +0200

    having count > 1 for edge cases handeld + code cleaning
    Co-authored-by: MO <MAbdulfattah@users.noreply.github.com>
    Co-authored-by: DaniThijsse <DaniThijsse@users.noreply.github.com>

--- util/helpers.py
+++ util/helpers.py
@@ -5,12 +5,10 @@ import numpy as np
 from matplotlib.pyplot import hist
 import random
 
-# def parseTriple(triple):
-#     ''' Makes a human readable string out of a single triple of URI's'''
-#     s = triple[0].split('/')[-1]
-#     p = triple[1].split('/')[-1].split('#')[-1]
-#     o = triple[2].split('/')[-1]
-#     return (s, p, o)
+"""
+TODO: Document
+
+"""
 
 def createLabel(s):
     return s['value'].replace('_',' ')
@@ -31,8 +29,8 @@ def parsePrefixes(triple):
 
 def parseTriple(triple):
     ''' Makes a human readable string out of a single triple of URI's'''
-    if type(triple) == 'tuple':    
-        print(triple)
+    # if type(triple) == 'tuple':    
+    #     print(triple)
     return [v.get('value').split('/')[-1].split('#')[-1] for v in triple]
 
 
@@ -88,12 +86,6 @@ def load_bot(name):
     bot = getattr(module, f'{name}Bot')
     return bot
 
-
-# api = api.API()
-# this is a first step to entropy
-# However, this actually finds the most common p o combinations, recursively
-
-# rescursive_query([Hisham, a, Human])
 def rescursiveQuery(state, split=0.5, depth=0):
     api = state.api
     prefixes = '\n'.join(api.prefixes)
@@ -116,19 +108,26 @@ def rescursiveQuery(state, split=0.5, depth=0):
         WHERE {{?s ?p ?o .
         {addFilterSPARQL(noHints=state.noHints)} }}
         GROUP BY ?p ?o
-        having(?poCount > 10) 
+        having(?poCount > 1) 
         ORDER BY DESC (?poCount)
         """
     qres = api.queryKG(query=query)
+    if not qres and not state.yesHints:
+        query = f"""
+        {prefixes}
+        SELECT distinct ?p ?o (count(?s) as ?poCount)
+        WHERE {{?s ?p ?o .
+        {addFilterSPARQL(noHints=state.noHints)} }}
+        GROUP BY ?p ?o
+        ORDER BY DESC (?poCount)
+        """
     qres = api.parseJSON(qres, [['poCount', 'p', 'o']])
     result = qres # select based on the split
     totalCount = getCurrentCount(state, api)
     a = np.array([int(x[0]['value']) for x in result])
     if np.all(a == 1): # This means that the bot found one specific subject, and there is only one label!
         labels = list(filter(lambda x: x[1]['value'] == 'label', qres))
-        print('Found only labels!', query)
         return random.choice(labels)
-    # print('count ', totalCount )
     
     best = min(qres, key=lambda x: abs(int(x[0]['value']) - int(totalCount) * split))
     if depth > 0:
