fa042eccdeb973643468cfb723085593ff29e2ff
Hisham Alkaed <heshamkaed2@gmail.com>
2022-04-02 17:18:16 +0200

    initializeState properly + filter sameAs and image for  human player
    Co-authored-by: MO <MAbdulfattah@users.noreply.github.com>
    Co-authored-by: DaniThijsse <DaniThijsse@users.noreply.github.com>

--- engine/state/main.py
+++ engine/state/main.py
@@ -1,17 +1,16 @@
-from statistics import variance
 from util import helpers, api
 """
 TODO: Document
 
 """
 class State:
-    def __init__(self, depth=0):
+    def __init__(self, depth=0, initializeState = True):
         self.api = api.API()
-        # TODO: only initialize the graph if needed (does the questioner allow it?!)
-        results = self.api.queryKG()
-        self.graph = self.api.parseJSON(results)
-        # self.graph = []
-        self.subGraphs = []
+        if not initializeState:
+            self.graph = []
+        else:
+            results = self.api.queryKG()
+            self.graph = self.api.parseJSON(results)
         self.currentDepth = depth
         self.yesHints = []
         self.noHints = []
@@ -22,6 +21,10 @@ class State:
 
       # update the state of the game.
     def update(self, question, answer):
+        """
+        TODO: Document
+
+        """
         self.questionsAsked += 1
 
         answer = answer.lower()
@@ -36,22 +39,24 @@ class State:
             self.yesHints.append(question)
         elif answer == 'no':
             self.noHints.append(question)
-
     # Updates graph each time an answer is given
 
     def updateGraph(self, question, answer):
-        # algorithm for updating the 
-        # ...
-        #
-        # print("You said {}".format(answer))
+        """
+        TODO: Document
+
+        """
         self.createSubGraph()
 
     def createSubGraph(self):
         """
+        TODO: Document
+
+        """
+        """
         Minimize the graph each time an answer is given. This function makes use of yesHints and noHints to query the graph.
         """
         prefixes = self.api.prefixes # [f'PREFIX {x}: <{prefix}>' for (prefix, x) in self.api.prefixes.items()]
-        
         query = """
         %s
             select *
@@ -71,13 +76,6 @@ class State:
                                 BIND(<http://www.w3.org/1999/02/22-rdf-syntax-ns#type> AS ?p) """%('\n'.join(prefixes))+ \
                                 helpers.addFilterSPARQL(noHints = self.noHints) +"}"
         
-        # print(query)
         results = self.api.queryKG(query)
         subGraph = self.api.parseJSON(results, [['s', 'p', 'o']])
-        self.subGraphs.append(subGraph)
         self.graph = subGraph
-
-        # for spo in subGraph:
-        #     s,p,o = helpers.parseTriple(spo)
-        #     if p == 'sameAs':
-        #         print('same as is here!!!')
