3021ab0bb5b2ecc47df8059b7743984e3552df87
hasan-sh <hasansh343@gmail.com>
2022-04-20 13:13:34 +0200

    Install needed packages

--- install-packages.sh
+++ install-packages.sh
@@ -2,6 +2,10 @@
 echo "You are in a working virtualenv $VIRTUAL_ENV";
 echo "Installing packages..";
 pip install numpy SPARQLWrapper 
+pip install wikipedia-api spacy transformers sentence_transformers pytest
+# pip3 uninstall torch torchvision torchaudio #--extra-index-url https://download.pytorch.org/whl/cu113
+python3 -m spacy download en_core_web_lg
+pip install git+https://github.com/thunlp/OpenNRE
 echo "Done!"
 echo "Tip: run 'python play.py' to start your first game!"
 exit 0;
